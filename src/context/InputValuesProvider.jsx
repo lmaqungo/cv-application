612e9eeafc20df06aaa3365a2f63f2075839f321
luya <91703950+lmaqungo@users.noreply.github.com>
2026-06-08 17:48:44 +0200

    apply default value on page load
    input state variables are now initialised with the default data

--- src/context/InputValuesProvider.jsx
+++ src/context/InputValuesProvider.jsx
@@ -1,22 +1,48 @@
 import {  useState, useRef } from "react";
 import { InputValuesContext } from "./InputValuesContext";
+import defaultPerson from "../default";
+import { templateEducation, templateJob, templateSkill, templateSection, templateSectionItem, templateLinkItem } from "../templates";
 
 export default function InputValuesProvider({children}){
     const [person, setPerson] = useState({
-        firstName:'',
-        lastName:'', 
-        jobTitle:'', 
-        phone:'', 
-        email:'', 
-        github:'',
-        website:'', 
-        location:'', 
-        profileSummary: ''
+        firstName: defaultPerson.firstName, 
+        lastName: defaultPerson.lastName, 
+        jobTitle: defaultPerson.jobTitle, 
+        phone: defaultPerson.phoneNumber, 
+        email: defaultPerson.emailAddress, 
+        github: defaultPerson.github,
+        website: defaultPerson.website, 
+        location: defaultPerson.location, 
+        profileSummary: defaultPerson.profileSummary
     })
-    const [jobs, setJobs] = useState([]); 
-    const [education, setEducation] = useState([]); 
-    const [skills, setSkills] = useState([]);
-    const [sections, setSections] = useState([]);
+    const [jobs, setJobs] = useState(
+        defaultPerson.jobs.map(job=> templateJob(
+            job.position, 
+            job.company, 
+            job.startDate, 
+            job.endDate, 
+            job.description         
+        ))
+    ); 
+    const [education, setEducation] = useState(
+        defaultPerson.education.map(edu=> templateEducation(
+            edu.school, 
+            edu.course, 
+            edu.startDate, 
+            edu.endDate, 
+            edu.description
+        ))
+    ); 
+    const [skills, setSkills] = useState(
+        defaultPerson.skills.map(skill => templateSkill(skill))
+    );
+    const [sections, setSections] = useState(
+        defaultPerson.sections.map(section=> templateSection(
+            section.sectionName, 
+            section.sectionItems.map(sectionItem => templateSectionItem(sectionItem)), 
+            section.linkItems.map(linkItem => templateLinkItem(linkItem.href, linkItem.linkContent))
+        ))
+    );
     const [toggleEdit, setToggleEdit] = useState(true);
     const contentRef = useRef(null);
 
